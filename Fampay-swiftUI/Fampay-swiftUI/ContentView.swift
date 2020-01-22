4b8ef9a2d284f0ef2e867ad3e2a96c8a0d5e23f2
Anshul Shah <anshul.shah@jump360.me>
2020-01-22 09:53:09 +0530

    Initial project commit

---
+++ Fampay-swiftUI/Fampay-swiftUI/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Fampay-swiftUI
+//
+//  Created by Anshul on 22/01/20.
+//  Copyright © 2020 Anshul Shah. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
